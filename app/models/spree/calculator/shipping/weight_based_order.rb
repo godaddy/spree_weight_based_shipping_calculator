62d54dad21e75a3474a2944563cb379686bb12be
Alan Lo <alanlo@godaddy.com>
2014-05-11 15:27:23 -0700

    Add validation rules.

--- app/models/spree/calculator/shipping/weight_based_order.rb
+++ app/models/spree/calculator/shipping/weight_based_order.rb
@@ -8,13 +8,14 @@ module Spree
                :dependent => :destroy
 
       accepts_nested_attributes_for :rates,
-                                    :allow_destroy => true,
-                                    :reject_if => proc { |attr| attr[:from_value].blank? && attr[:rate].blank? }
+                                    :allow_destroy => true
 
       # If weight is not defined for an item, use this instead
       preference :default_item_weight, :decimal, :default => 0
 
       attr_accessible :rates_attributes
+
+      validate :validate_at_least_one_rate, :validate_rates_uniqueness
 
       def self.description
         Spree.t(:weight_based_shipping_rate_per_order)
@@ -56,6 +57,15 @@ module Spree
       # Get the rate from the database or nil if could not find the rate
       def get_rate(value)
         Spree::WeightBasedCalculatorRate.find_rate(self.id, value)
+      end
+
+      def validate_at_least_one_rate
+        errors.add(:rates, "must have at least one") unless rates.length > 0
+      end
+
+      def validate_rates_uniqueness
+        new_or_existing_rates = rates.reject { |r| r.marked_for_destruction? }
+        errors.add(:rates, "must be unique")  if new_or_existing_rates.map(&:from_value).uniq.length < new_or_existing_rates.length
       end
     end
   end
